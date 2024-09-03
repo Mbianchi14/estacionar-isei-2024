4c53eb0aff376c8a190bab447eab26acdf538405
Maximo <maboggino@gmail.com>
2024-09-03 14:48:10 -0300

    Commit 2409031449

---
+++ promo.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="./assets/css/main.css">
+
+<div class="d-flex justify-content-center  mt-5">
+    <div class="d-flex justify-content-left bg-azul-200 p-5 radio-lg col-10">
+        <div class="d-flex justify-content-center bg-azul-900 p-1 radio-small col-1" style="max-height: 55px;max-width: 55px;">
+            <img class="col-12" src="./assets/img/close.png" alt="X">
+        </div>
+        <div class="pl-2 col-7">
+            <span style="font-size:20px;color:gray;">Promo Invierno</span>
+            <p style="font-size:30px;" class="my-1">De 15hs a 22hs</p>
+            <p style="font-size:30px;" class="my-1">Del 15/04/24 al 16/08/24</p>
+            <p style="font-size:30px;font-weight: bold;" class="mt-1">20% de Descuento</p>
+        </div>
+        <div class="d-flex justify-content-between align-items-center col-4">
+            <div class="col-8 alert-success radio-small d-flex justify-content-center p-1" style="font-size:30px;font-weight: bold;">Activa</div>
+            <img class="col-4 d-flex justify-content-center" src="./assets/img/flechitaGris.png" alt=">">
+        </div>
+    </div>
+</div>
+
+<div class="d-flex justify-content-center mt-5">
+    <div class="d-flex justify-content-left bg-azul-200 p-5 radio-lg col-10">
+        <div class="d-flex justify-content-center bg-azul-900 p-1 radio-small col-1" style="max-height: 55px;max-width: 55px;">
+            <img class="col-12" src="./assets/img/close.png" alt="X">
+        </div>
+        <div class="pl-2 col-7">
+            <span style="font-size:20px;color:gray;">Promo Invierno</span>
+            <p style="font-size:30px;" class="my-1">De 15hs a 22hs</p>
+            <p style="font-size:30px;" class="my-1">Del 15/04/24 al 16/08/24</p>
+            <p style="font-size:30px;font-weight: bold;" class="mt-1">20% de Descuento</p>
+        </div>
+        <div class="d-flex justify-content-between align-items-center col-4">
+            <div class="col-8 alert-danger radio-small d-flex justify-content-center p-1" style="font-size:30px;font-weight: bold;">Inactiva</div>
+            <img class="col-4 d-flex justify-content-center" src="./assets/img/flechitaGris.png" alt=">">
+        </div>
+    </div>
+</div>
