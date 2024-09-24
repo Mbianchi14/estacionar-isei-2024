3738e144fc7a29ac016c20b525ab21ea61c81853
lucas <lucasrapaport195@gmail.com>
2024-09-24 20:10:51 -0300

    Commit2409242010

---
+++ horarios.php
@@ -0,0 +1,73 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="./assets/css/main.css">
+
+<div class="d-flex justify-content-center  mt-5">
+    <div class="bg-azul-200 p-5 radio-lg col-10">
+        <div class="col-12 d-flex justify-content-left ">
+            <div class="col-7">
+                <span style="font-size:20px;color:gray;">Día de Semana</span>
+                <p style="font-size:30px;" class="my-1">De 10hs a 22hs</p>
+                <p style="font-size:30px;" class="my-1">Del 15/04/24 al 16/08/24</p>
+            </div>
+            <div class="d-flex justify-content-between align-items-center col-5">
+                <div class="col-8 alert-success radio-small d-flex justify-content-center p-1" style="font-size:30px;font-weight: bold;">Activa</div>
+                <img class="col-4 d-flex justify-content-center" src="./assets/img/flechitaGris.png" alt=">">
+            </div>
+        </div>
+        <div class="col-12 d-flex justify-content-between pt-4">
+            <div class="d-flex justify-content-center align-items-center radio-small bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;width:2em;height:2em;">L</div>
+            <div class="d-flex justify-content-center align-items-center radio-small bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;width:2em;height:2em;">M</div>
+            <div class="d-flex justify-content-center align-items-center radio-small bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;width:2em;height:2em;">X</div>
+            <div class="d-flex justify-content-center align-items-center radio-small bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;width:2em;height:2em;">J</div>
+            <div class="d-flex justify-content-center align-items-center radio-small bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;width:2em;height:2em;">V</div>
+            <div class="d-flex justify-content-center align-items-center radio-small text-azul-900 p-1 bg-white" style="font-size:30px;font-weight: bold;width:2em;height:2em;">S</div>
+            <div class="d-flex justify-content-center align-items-center radio-small text-azul-900 p-1 bg-white" style="font-size:30px;font-weight: bold;width:2em;height:2em;">D</div>
+        </div>
+    </div>
+    <!-- <div class="d-flex justify-content-left bg-azul-200 p-5 radio-lg col-10">
+        <div class="pl-2 col-7">
+            <span style="font-size:20px;color:gray;">Día de Semana</span>
+            <p style="font-size:30px;" class="my-1">De 10hs a 22hs</p>
+            <p style="font-size:30px;" class="my-1">Del 15/04/24 al 16/08/24</p>
+            <p style="font-size:30px;font-weight: bold;" class="mt-1">20% de Descuento</p>
+            <div class="col-12 d-flex justify-content-between">
+                <div class="col d-flex justify-content-center bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;border-radius:100%;">L</div>
+                <div class="col d-flex justify-content-center bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;border-radius:100%;">M</div>
+                <div class="col d-flex justify-content-center bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;border-radius:100%;">X</div>
+                <div class="col d-flex justify-content-center bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;border-radius:100%;">J</div>
+                <div class="col d-flex justify-content-center bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;border-radius:100%;">V</div>
+                <div class="col d-flex justify-content-center text-azul-900 p-1 bg-white"  style="font-size:30px;font-weight: bold;border-radius:100%;">S</div>
+                <div class="col d-flex justify-content-center text-azul-900 p-1 bg-white"  style="font-size:30px;font-weight: bold;border-radius:100%;">D</div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-between align-items-center col-5">
+            <div class="col-8 alert-success radio-small d-flex justify-content-center p-1" style="font-size:30px;font-weight: bold;">Activa</div>
+            <img class="col-4 d-flex justify-content-center" src="./assets/img/flechitaGris.png" alt=">">
+        </div>
+    </div> -->
+</div>
+
+<div class="d-flex justify-content-center mt-5">
+    <div class="bg-azul-200 p-5 radio-lg col-10">
+        <div class="col-12 d-flex justify-content-left ">
+            <div class="col-7">
+                <span style="font-size:20px;color:gray;">Finde</span>
+                <p style="font-size:30px;" class="my-1">De 10hs a 22hs</p>
+                <p style="font-size:30px;" class="my-1">Del 15/04/24 al 16/08/24</p>
+            </div>
+            <div class="d-flex justify-content-between align-items-center col-5">
+                <div class="col-8 alert-danger radio-small d-flex justify-content-center p-1" style="font-size:30px;font-weight: bold;">Inactiva</div>
+                <img class="col-4 d-flex justify-content-center" src="./assets/img/flechitaGris.png" alt=">">
+            </div>
+        </div>
+        <div class="col-12 d-flex justify-content-between pt-4">
+            <div class="d-flex justify-content-center align-items-center radio-small text-azul-900 p-1 bg-white" style="font-size:30px;font-weight: bold;width:2em;height:2em;">L</div>
+            <div class="d-flex justify-content-center align-items-center radio-small text-azul-900 p-1 bg-white" style="font-size:30px;font-weight: bold;width:2em;height:2em;">M</div>
+            <div class="d-flex justify-content-center align-items-center radio-small text-azul-900 p-1 bg-white" style="font-size:30px;font-weight: bold;width:2em;height:2em;">X</div>
+            <div class="d-flex justify-content-center align-items-center radio-small text-azul-900 p-1 bg-white" style="font-size:30px;font-weight: bold;width:2em;height:2em;">J</div>
+            <div class="d-flex justify-content-center align-items-center radio-small text-azul-900 p-1 bg-white" style="font-size:30px;font-weight: bold;width:2em;height:2em;">V</div>
+            <div class="d-flex justify-content-center align-items-center radio-small bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;width:2em;height:2em;">S</div>
+            <div class="d-flex justify-content-center align-items-center radio-small bg-azul-900 p-1" style="font-size:30px;font-weight: bold;color:white;width:2em;height:2em;">D</div>
+        </div>
+    </div>
+</div>
